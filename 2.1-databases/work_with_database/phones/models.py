8aa349b855a635200671878b548e734475645020
Beda3113 <130255340+Beda3113@users.noreply.github.com>
2025-03-02 00:01:53 +0300

    Update models.py

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,19 @@
 from django.db import models
-
+from django.utils.text import slugify
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    id = models.AutoField(primary_key=True)  # Основной ключ модели
+    name = models.CharField(max_length=255)  # Название телефона
+    price = models.DecimalField(max_digits=10, decimal_places=2)  # Цена
+    image = models.URLField()  # Ссылка на изображение
+    release_date = models.DateField()  # Дата выпуска
+    lte_exists = models.BooleanField()  # Наличие LTE
+    slug = models.SlugField(max_length=255, unique=True, blank=True)  # Slug для URL
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)  # Генерация slug из названия
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
