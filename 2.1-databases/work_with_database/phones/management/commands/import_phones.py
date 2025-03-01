557836b78b21538b4fe2e0d8a269989dfcf32f45
Beda3113 <130255340+Beda3113@users.noreply.github.com>
2025-03-02 00:12:30 +0300

    Update import_phones.py

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -1,17 +1,30 @@
 import csv
-
 from django.core.management.base import BaseCommand
 from phones.models import Phone
 
 
 class Command(BaseCommand):
+    help = 'Импорт телефонов из CSV-файла'
+
     def add_arguments(self, parser):
-        pass
+        parser.add_argument('csv_file', type=str, help='Путь к CSV-файлу')
 
     def handle(self, *args, **options):
-        with open('phones.csv', 'r') as file:
+        csv_file_path = options['csv_file']
+
+        with open(csv_file_path, 'r', encoding='utf-8') as file:
             phones = list(csv.DictReader(file, delimiter=';'))
 
-        for phone in phones:
-            # TODO: Добавьте сохранение модели
-            pass
+        for phone_data in phones:
+            # Преобразуем данные из CSV в формат, подходящий для модели Phone
+            phone = Phone(
+                id=phone_data['id'],
+                name=phone_data['name'],
+                price=float(phone_data['price']),  # Цена должна быть числом
+                image=phone_data['image'],
+                release_date=phone_data['release_date'],  # Дата выпуска
+                lte_exists=(phone_data['lte_exists'].lower() == 'true')  # Преобразуем строку в boolean
+            )
+            phone.save()  # Сохраняем объект в базу данных
+
+        self.stdout.write(self.style.SUCCESS('Телефоны успешно импортированы'))
