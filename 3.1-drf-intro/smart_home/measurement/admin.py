f53ce0ad93150ce202559be075b13b75001954ad
Beda3113 <130255340+Beda3113@users.noreply.github.com>
2025-04-08 09:02:55 +0300

    Update admin.py

--- 3.1-drf-intro/smart_home/measurement/admin.py
+++ 3.1-drf-intro/smart_home/measurement/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Sensor, Measurement
 
-# Register your models here.
+@admin.register(Sensor)
+class SensorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description')
+    search_fields = ('name', 'description')
+    list_filter = ('name',)
+
+@admin.register(Measurement)
+class MeasurementAdmin(admin.ModelAdmin):
+    list_display = ('id', 'sensor', 'temperature', 'created_at')
+    search_fields = ('sensor__name',)
+    list_filter = ('sensor', 'created_at')
