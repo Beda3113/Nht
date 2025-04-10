064a2ecee90fbcdda7735a3f48a3fbf7cf72f19e
Beda3113 <130255340+Beda3113@users.noreply.github.com>
2025-04-10 04:10:31 +0300

    Update admin.py

--- 3.3-permissions/api_with_restrictions/advertisements/admin.py
+++ 3.3-permissions/api_with_restrictions/advertisements/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Advertisement
 
-# Register your models here.
+@admin.register(Advertisement)
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'creator', 'status', 'created_at', 'updated_at')
+    list_display_links = ('id', 'title')
+    list_filter = ('status', 'created_at', 'creator')
+    search_fields = ('title', 'description', 'creator__username')
+    list_editable = ('status',)
+    date_hierarchy = 'created_at'
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'description', 'creator')
+        }),
+        ('Status', {
+            'fields': ('status',),
+            'classes': ('collapse',)
+        }),
+        ('Dates', {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',),
+        }),
+    )
+    readonly_fields = ('created_at', 'updated_at')
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(creator=request.user)
