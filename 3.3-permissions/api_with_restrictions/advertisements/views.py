a5ac1a3f8c313f9f22033ff4c837782850fc8132
Beda3113 <130255340+Beda3113@users.noreply.github.com>
2025-04-10 04:07:15 +0300

    Update views.py

--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -1,15 +1,39 @@
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.throttling import UserRateThrottle, AnonRateThrottle
+from django_filters.rest_framework import DjangoFilterBackend
 
+from .models import Advertisement
+from .serializers import AdvertisementSerializer
+from .filters import AdvertisementFilter
+from .permissions import IsOwnerOrAdmin
 
-class AdvertisementViewSet(ModelViewSet):
-    """ViewSet для объявлений."""
-
-    # TODO: настройте ViewSet, укажите атрибуты для кверисета,
-    #   сериализаторов и фильтров
+class AdvertisementViewSet(viewsets.ModelViewSet):
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = AdvertisementFilter
+    throttle_classes = [UserRateThrottle, AnonRateThrottle]
 
     def get_permissions(self):
-        """Получение прав для действий."""
-        if self.action in ["create", "update", "partial_update"]:
-            return [IsAuthenticated()]
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            return [IsAuthenticated(), IsOwnerOrAdmin()]
         return []
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.request.user.is_authenticated:
+            return queryset
+        return queryset.exclude(status=AdvertisementStatusChoices.DRAFT)
+
+    def perform_create(self, serializer):
+        serializer.save(creator=self.request.user)
+
+    @action(detail=True, methods=['post'])
+    def close(self, request, pk=None):
+        advertisement = self.get_object()
+        advertisement.status = AdvertisementStatusChoices.CLOSED
+        advertisement.save()
+        return Response({'status': 'Объявление закрыто'})
