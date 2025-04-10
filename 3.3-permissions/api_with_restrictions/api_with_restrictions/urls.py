ea437c27efcf67d1e48abe13e880ec170e5a6b69
Beda3113 <130255340+Beda3113@users.noreply.github.com>
2025-04-10 04:12:35 +0300

    Update urls.py

--- 3.3-permissions/api_with_restrictions/api_with_restrictions/urls.py
+++ 3.3-permissions/api_with_restrictions/api_with_restrictions/urls.py
@@ -15,14 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
 from rest_framework.routers import DefaultRouter
+from advertisements.views import AdvertisementViewSet
 
 router = DefaultRouter()
-# TODO: подключите `AdvertisementViewSet`
-
+router.register('advertisements', AdvertisementViewSet, basename='advertisements')
 
 urlpatterns = [
     path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls')),  # Добавляем URL для авторизации через сессии
     path('admin/', admin.site.urls),
 ]
