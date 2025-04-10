eb61a56cee98e764c3988c668239884a59c85c0f
Beda3113 <130255340+Beda3113@users.noreply.github.com>
2025-04-10 04:09:37 +0300

    Update filters.py

--- 3.3-permissions/api_with_restrictions/advertisements/filters.py
+++ 3.3-permissions/api_with_restrictions/advertisements/filters.py
@@ -1,12 +1,30 @@
 from django_filters import rest_framework as filters
+from django_filters.widgets import RangeWidget
+from django.contrib.auth.models import User
 
 from advertisements.models import Advertisement
 
 
 class AdvertisementFilter(filters.FilterSet):
     """Фильтры для объявлений."""
-
-    # TODO: задайте требуемые фильтры
+    
+    created_at = filters.DateFromToRangeFilter(
+        field_name='created_at',
+        label='Дата создания (диапазон)',
+        widget=RangeWidget(attrs={'type': 'date'})
+    )
+    
+    creator = filters.ModelChoiceFilter(
+        field_name='creator',
+        queryset=User.objects.all(),
+        label='Создатель'
+    )
+    
+    status = filters.ChoiceFilter(
+        choices=Advertisement.STATUS_CHOICES,
+        label='Статус'
+    )
 
     class Meta:
         model = Advertisement
+        fields = ['created_at', 'creator', 'status']
