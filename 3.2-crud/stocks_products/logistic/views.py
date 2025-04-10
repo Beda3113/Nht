9bacb41d5e18895f2cf19c40da5322e3afda60cb
Beda3113 <130255340+Beda3113@users.noreply.github.com>
2025-04-10 03:58:39 +0300

    Update views.py

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -1,16 +1,23 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.pagination import LimitOffsetPagination
 
-from logistic.models import Product, Stock
-from logistic.serializers import ProductSerializer, StockSerializer
+from .models import Product, Stock
+from .serializers import ProductSerializer, StockSerializer
 
 
-class ProductViewSet(ModelViewSet):
+class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    # при необходимости добавьте параметры фильтрации
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'description']
+    pagination_class = LimitOffsetPagination
 
 
-class StockViewSet(ModelViewSet):
-    queryset = Stock.objects.all()
+class StockViewSet(viewsets.ModelViewSet):
+    queryset = Stock.objects.prefetch_related('positions__product').all()
     serializer_class = StockSerializer
-    # при необходимости добавьте параметры фильтрации
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['products']
+    search_fields = ['positions__product__title', 'positions__product__description']
+    pagination_class = LimitOffsetPagination
